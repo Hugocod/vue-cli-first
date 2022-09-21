2ab324db4a0fde6d5c559a7b8546f65b3d2a5239
Hugocod <u.medioli@icloud.com>
2022-09-21 14:02:52 +0200

    done

---
+++ vue-test-app/src/App.vue
@@ -0,0 +1,51 @@
+<template>
+    <div id="app">
+        <img alt="Vue logo" src="./assets/logo.webp" />
+        <MyTitle msg="Viva React! :P" />
+        <JumboTron />
+    </div>
+</template>
+
+<script>
+import MyTitle from "./components/MyTitle";
+import JumboTron from "./components/JumboTron";
+
+export default {
+    name: "App",
+    components: {
+        MyTitle,
+        JumboTron,
+    },
+};
+</script>
+
+<style lang="scss">
+#app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+
+    height: 100vh;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+
+    img {
+        animation: spin 5s infinite;
+    }
+
+    @keyframes spin {
+        from {
+            transform: rotate(0deg);
+        }
+        to {
+            transform: rotate(360deg);
+        }
+    }
+}
+</style>
---
+++ vue-test-app/src/components/JumboTron.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="jumbo">
+        <h1>io sono un jumbotron (credo)</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "JumboTron",
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.jumbo {
+    background-color: rgb(181, 222, 121);
+
+    padding: 30px;
+    border-radius: 2rem;
+    h1 {
+        font-size: 3rem;
+        color: white;
+    }
+}
+</style>
